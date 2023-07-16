95b3172222b8bfb8f03748eae59e8f8c270f19fa
ngr-np <ngr112001@gmail.com>
2023-07-16 12:42:06 +0545

    commit

--- src/app/(adminPages)/layout.tsx
+++ src/app/(adminPages)/layout.tsx
@@ -1,6 +1,10 @@
 import AdminAsideSec from "@/sections/adminSections/AdminAsideSec";
 import React from "react";
-
+export const metadata = {
+  title: "Dashboard",
+  description:
+    "second hand bike, scooter, e-bike buying and selling marketplace",
+};
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
   return <AdminAsideSec>{children}</AdminAsideSec>;
 };
