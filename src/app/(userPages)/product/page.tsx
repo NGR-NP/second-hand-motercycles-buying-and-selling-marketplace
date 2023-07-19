109bfb50f5e53d9947da72358de0416cffc6df95
Roshan Chaudhary <rosan.91111@gmail.com>
2023-07-19 12:54:30 +0545

    asjfncds

--- src/app/(userPages)/product/page.tsx
+++ src/app/(userPages)/product/page.tsx
@@ -1,12 +1,12 @@
 import NavBar from "@/sections/NavBar";
 import React from "react";
+import Image from "next/image";
 import { BiSolidColor } from "react-icons/bi";
 import { MdVerified } from "react-icons/md";
 import { AiOutlineHeart } from "react-icons/ai";
-import { PiEngineThin } from "react-icons/pi";
+import { PiEngineFill } from "react-icons/pi";
 import { IoFlashSharp } from "react-icons/io5";
 import { GiStopwatch, GiSpeedometer } from "react-icons/gi";
-import Image from "next/image";
 
 const ProductPage = () => {
   return (
@@ -87,16 +87,12 @@ const ProductPage = () => {
             </div>
             <div className="flex space-x-12">
               <div>
-                <button
-                  className="p-2 w-40 font-semibold text-center text-white rounded-2xl font-inter bg-secondary hover:shadow-md"
-                >
+                <button className="p-2 w-40 font-semibold text-center text-white rounded-2xl font-inter bg-secondary hover:shadow-md">
                   BUY NOW
                 </button>
               </div>
               <div>
-                <button
-                  className="flex justify-center p-2 w-40 font-semibold rounded-2xl border font-inter text-secondary border-secondary hover:shadow-md"
-                >
+                <button className="flex justify-center p-2 w-40 font-semibold rounded-2xl border font-inter text-secondary border-secondary hover:shadow-md">
                   <div className="flex items-center space-x-2">
                     <div>
                       <AiOutlineHeart />
@@ -113,7 +109,7 @@ const ProductPage = () => {
           <div className="flex mr-4 ml-4 space-x-24">
             <div className="flex flex-col items-center">
               <div>
-                <PiEngineThin size={50} />
+                <PiEngineFill size={50} />
               </div>
               <div className="text-xs text-slate-600">Engine</div>
               <div className="text-base">155 cc</div>
