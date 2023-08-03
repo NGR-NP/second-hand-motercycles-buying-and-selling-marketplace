f512e0d2ed1e52e834639119e944b7443a2e6f25
Roshan Chaudhary <rosan.91111@gmail.com>
2023-08-03 13:35:04 +0545

    commit

--- src/app/(userPages)/blog/page.tsx
+++ src/app/(userPages)/blog/page.tsx
@@ -3,28 +3,42 @@ import React, { useEffect, useState } from "react";
 
 const API = "https://648ec59675a96b664444425e.mockapi.io";
 
+type APITypes = {
+  id?: number;
+  createdAt: number;
+  avatar: any[];
+  title: string;
+  description: string;
+  author: string;
+};
+
 const Blog = () => {
   const [datas, setData] = useState([]);
 
   const FetchData = async () => {
-    const req = await fetch(`${API}/Blog`);
-    const res = await req.json();
-    setData(res);
-    console.log(res);
+    try {
+      const req = await fetch(`${API}/Blog`);
+      const res = await req.json();
+      setData(res);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
   };
+
   useEffect(() => {
     FetchData();
   }, []);
 
   return (
-    <div className="mt-28 mb-5 flex gap-40 p-10">
+    <div className="mt-28 mb-5 flex justify-between p-10">
       <div className="flex flex-col gap-5">
         <div className="h-60 w-96 rounded-xl shadow-2xl flex">
           <div className="h-full">
             <img src="" alt="image" />
             <p>
-              {datas.map((data) => (
-                <div key={data}>{}</div>
+              {datas.map((data, idx) => (
+                <div key={idx}>{data}</div>
               ))}
             </p>
           </div>
